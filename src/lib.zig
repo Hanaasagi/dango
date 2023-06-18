e739b5f0b8b4d29727ab1f7998d133324b9d7af6
Hanaasagi <ambiguous404@gmail.com>
2023-06-19 00:15:52 +0900

    feat: try to impl a map

--- src/lib.zig
+++ src/lib.zig
@@ -1,10 +1,27 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+fn map(comptime Src: type, comptime Dst: type, comptime len: usize, array: [len]Src, function: fn (Src) Dst) [len]Dst {
+    var result: [len]Dst = undefined;
+    var index: usize = 0;
+    while (index < len) {
+        result[index] = function(array[index]);
+        index += 1;
+    }
+    return result;
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "test map" {
+    var array_thing = map(
+        f64,
+        f64,
+        3,
+        .{ 1.0, 2.0, 3.0 },
+        struct {
+            fn add(a: f64) f64 {
+                return a + 1.0;
+            }
+        }.add,
+    );
+    try testing.expectEqualSlices(f64, &[3]f64{ 2.0, 3.0, 4.0 }, &array_thing);
 }
